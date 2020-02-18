321421d3e82ddb23ab8b5195646a41e3d19ad33c
Bryant Omoregie <unclebryant@gmail.com>
2020-02-18 20:01:55 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,6 +43,9 @@ def naming_system
           label: "Family",
            sub_category: {
              label: "Genus",
+              sub_category: {
+                label: "Species"
+              }
              
            }
         }
